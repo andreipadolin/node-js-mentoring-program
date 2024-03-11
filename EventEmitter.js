58cabef1b0ca8bb5f08e8836902376d39b0669cb
Andrei Padolin <apadolin@infoblox.com>
2024-03-11 23:27:17 +0200

    task31

--- EventEmitter.js
+++ EventEmitter.js
@@ -42,15 +42,42 @@ module.exports = class EventEmitter {
     return this;
   }
 
-  removeEventListener(eventName, fn) {
-    return this.off(eventName, fn);
+  off(eventName, fn) {
+    return this.removeListener(eventName, fn);
   }
 
-  once(eventName, fn) {}
+  once(eventName, fn) {
+    this.listeners[eventName] = this.listeners[eventName] || [];
+    const onceWrapper = () => {
+      fn();
+      this.removeListener(eventName, onceWrapper);
+    };
+    //this.listeners[eventName].push(onceWrapper);
+    this.addListener(eventName, onceWrapper);
+    return this;
+  }
 
-  emit(eventName, ...args) {}
+  // not sure about return value
+  emit(eventName, ...args) {
+    if (!eventName) {
+      throw new Error("Event name is required");
+    }
+    if (this.listeners[eventName]) {
+      this.listeners[eventName].forEach((listener) => listener(...args));
+    }
+  }
 
-  listenerCount(eventName) {}
+  listenerCount(eventName) {
+    if (!eventName) {
+      throw new Error("Event name is required");
+    }
+    return this.listeners[eventName] ? this.listeners[eventName].length : 0;
+  }
 
-  rawListeners(eventName) {}
+  rawListeners(eventName) {
+    if (!eventName) {
+      throw new Error("Event name is required");
+    }
+    return this.listeners[eventName] || [];
+  }
 };
